137af912ede88d58ca2b2661a7726e11857f70dd
Muninaik-mude <munikumar.m@orotron.com>
2026-03-02 14:53:21 +0530

    MKN -- Added internet permission

--- frontend/lib/core/constants/api_constants.dart
+++ frontend/lib/core/constants/api_constants.dart
@@ -3,9 +3,10 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class ApiConstants {
   ApiConstants._();
 
-  static String get baseUrl => dotenv.env['BASE_URL'] ?? 'http://localhost:8000/api/v1';
+  static String get baseUrl =>
+      dotenv.env['BASE_URL'] ?? 'https://e-commerce-23ff.onrender.com/api/v1';
 
-  static const int connectTimeout = 30000;
-  static const int receiveTimeout = 30000;
+  static const int connectTimeout = 60000;
+  static const int receiveTimeout = 60000;
   static const int maxRetries = 2;
 }
